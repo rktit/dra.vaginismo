6c691cd35194fc1716a59f274754eb1cafb24747
Clarisa-Rainet <82967363+Clarisa-Rainet@users.noreply.github.com>
2022-05-06 15:24:42 -0300

    obrigado ajuste

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,20 @@
-//  import SaibaMais from "./saiba-mais";
+ import SaibaMais from "./saiba-mais";
 
-import Topo from "./topo";
-import Reservar from "./reservar";
-import Line from "./line";
-import Video from "./video";
-import Biografia from "./biografia";
-import Footer from "./footer";
+// import Topo from "./topo";
+// import Reservar from "./reservar";
+// import Line from "./line";
+// import Video from "./video";
+// import Biografia from "./biografia";
+// import Footer from "./footer";
 
 
 export {
-// SaibaMais,
+SaibaMais,
 
-Topo,
-Reservar,
-Line,
-Video,
-Biografia,
-Footer,
+// Topo,
+// Reservar,
+// Line,
+// Video,
+// Biografia,
+// Footer,
 };
